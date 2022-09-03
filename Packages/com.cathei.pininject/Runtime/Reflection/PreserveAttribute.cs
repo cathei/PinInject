265c8dd7fb0a6fddb1181bf797fae8a34dbee732
Maxwell Keonwoo Kang <code.athei@gmail.com>
2022-09-02 21:42:07 -0700

    add main branch as submodule

--- Packages/com.cathei.pininject/Runtime/Reflection/PreserveAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-
-namespace Cathei.PinInject.Internal
-{
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-    public class PreserveAttribute : Attribute { }
-}
